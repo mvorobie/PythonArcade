c19df88a78ebca9087fa805b20c0cf30854d2c3f
Mikhail Vorobiev <vmishad@yahoo.com>
2022-09-16 14:25:17 +0200

    add movement from user input

--- arcade_platformer/platformer.py
+++ arcade_platformer/platformer.py
@@ -16,7 +16,8 @@ MAP_SCALING = 1.0
 GRAVITY = 1.0
 PLAYER_START_X = 65
 PLAYER_START_Y = 256
-
+PLAYER_MOVE_SPEED = 10
+PLAYER_JUMP_SPEED = 20
 
 # Assets path
 ASSETS_PATH = pathlib.Path(__file__).resolve().parent.parent / "assets"
@@ -183,7 +184,26 @@ class Platformer(arcade.Window):
             key {int} -- Which key was pressed
             modifiers {int} -- Which modifiers where down at the time
         """
-        pass
+        # Check for player left or right movement
+        if key in [arcade.key.LEFT, arcade.key.A]:
+            self.player.change_x = -PLAYER_MOVE_SPEED
+        elif key in [arcade.key.RIGHT, arcade.key.D]:
+            self.player.change_x = PLAYER_MOVE_SPEED
+
+        # Check if player can climb up or down
+        elif key in [arcade.key.UP, arcade.key.W]:
+            if self.physics_engine.is_on_ladder():
+                self.player.change_y = PLAYER_MOVE_SPEED
+        elif key in [arcade.key.DOWN, arcade.key.S]:
+            if self.physics_engine.is_on_ladder():
+                self.player.change_y = -PLAYER_MOVE_SPEED
+
+        # Check if player can jump
+        elif key == arcade.key.SPACE:
+            if self.physics_engine.can_jump():
+                self.player.change_y = PLAYER_JUMP_SPEED
+                # Play the jump sound
+                arcade.play_sound(self.jump_sound)
 
     def on_key_release(self, key: int, modifiers: int):
         """Processes key releases
@@ -192,7 +212,24 @@ class Platformer(arcade.Window):
             key {int} -- Which key was released
             modifiers {int} -- Which modifiers where down at the time
         """
-        pass
+        # Check for player left or right movement
+        if key in [
+            arcade.key.LEFT,
+            arcade.key.A,
+            arcade.key.RIGHT,
+            arcade.key.D,
+        ]:
+            self.player.change_x = 0
+
+        # Check if player can climb up or down
+        elif key in [
+            arcade.key.UP,
+            arcade.key.W,
+            arcade.key.DOWN,
+            arcade.key.S,
+        ]:
+            if self.physics_engine.is_on_ladder():
+                self.player.change_y = 0
 
     def on_update(self, delta_time: float):
         """Updates the position of all game objects
